666818a1da845cddc6072a24b15be49dbdfb6d32
AkshatTiwari343 <148128253+AkshatTiwari343@users.noreply.github.com>
2025-03-29 00:16:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
-//w
-document.querySelector('.w').addEventListener("click",w);
-function w(){
-  var audio= new Audio('./sounds/crash.mp3');
-  audio.play();
-}
+// //w
+// document.querySelector('.w').addEventListener("click",w);
+// function w(){
+//   var audio= new Audio('./sounds/crash.mp3');
+//   audio.play();
+// }
 
-//a
-document.querySelector('.a').addEventListener("click",a);
-function a(){
-  var audio= new Audio('./sounds/kick-bass.mp3');
-  audio.play();
-}
+// //a
+// document.querySelector('.a').addEventListener("click",a);
+// function a(){
+//   var audio= new Audio('./sounds/kick-bass.mp3');
+//   audio.play();
+// }
 
-//s
-document.querySelector('.s').addEventListener("click",s);
-function s(){
-  var audio= new Audio('./sounds/snare.mp3');
-  audio.play();
-}
+// //s
+// document.querySelector('.s').addEventListener("click",s);
+// function s(){
+//   var audio= new Audio('./sounds/snare.mp3');
+//   audio.play();
+// }
 
-//d
-document.querySelector('.d').addEventListener("click",d);
-function d(){
-  var audio= new Audio('./sounds/tom-1.mp3');
-  audio.play();
-}
+// //d
+// document.querySelector('.d').addEventListener("click",d);
+// function d(){
+//   var audio= new Audio('./sounds/tom-1.mp3');
+//   audio.play();
+// }
 
-//j
-document.querySelector('.j').addEventListener("click",j);
-function j(){
-  var audio= new Audio('./sounds/tom-2.mp3');
-  audio.play();
-}
+// //j
+// document.querySelector('.j').addEventListener("click",j);
+// function j(){
+//   var audio= new Audio('./sounds/tom-2.mp3');
+//   audio.play();
+// }
 
-//k
-document.querySelector('.k').addEventListener("click",k);
-function k(){
-  var audio= new Audio('./sounds/tom-3.mp3');
-  audio.play();
-}
+// //k
+// document.querySelector('.k').addEventListener("click",k);
+// function k(){
+//   var audio= new Audio('./sounds/tom-3.mp3');
+//   audio.play();
+// }
 
 
-//l
-document.querySelector('.l').addEventListener("click",l);
-function l(){
-  var audio= new Audio('./sounds/tom-4.mp3');
-  audio.play();
-}
+// //l
+// document.querySelector('.l').addEventListener("click",l);
+// function l(){
+//   var audio= new Audio('./sounds/tom-4.mp3');
+//   audio.play();
+// }
+var num =document.querySelectorAll(".drum").length;
+for(var i=0;i<num;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      var buttonInnerHtml= this.innerHTML;
+      makesound(buttonInnerHtml);
+    })
 
 function makeSound(key){
   switch(key){
@@ -84,4 +90,8 @@ function makeSound(key){
 document.addEventListener("keypress",keyPressed);
 function keyPressed(event){
   makeSound(event.key);
+  buttonAnimation(even.key);
+}
+function buttonAnimation(currentKey){
+  
 }
